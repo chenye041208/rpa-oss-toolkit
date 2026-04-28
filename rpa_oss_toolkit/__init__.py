1fb67647383ed677b91d9cacc58a06dc38ef1b68
chenye041208 <liuyuanpengliu@163.com>
2026-04-28 11:57:29 +0800

    chore: bump version to 1.1.5

--- rpa_oss_toolkit/__init__.py
+++ rpa_oss_toolkit/__init__.py
@@ -52,7 +52,7 @@
         session.upload("本地文件.txt", "上传路径/文件.txt")
 """
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 # 客户端与会话
 from .client import OSSClient, Session
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="rpa-oss-toolkit",
-    version="1.1.4",
+    version="1.1.5",
     packages=find_packages(),
     install_requires=[
         "oss2>=2.0.0",
